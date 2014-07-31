b71ad6bab66c36d311951a63ddabc759d63e104c
Richard Lesco <richardlesco@gmail.com>
2014-07-31 18:27:56 -0400

    New UICollectionViewCell subclass

---
+++ Reezy/RLReezyCell.h
@@ -0,0 +1,13 @@
+//
+//  RLReezyCell.h
+//  Reezy
+//
+//  Created by Richard on 2014-07-31.
+//  Copyright (c) 2014 Richard Lesco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RLReezyCell : UICollectionViewCell
+
+@end
---
+++ Reezy/RLReezyCell.m
@@ -0,0 +1,31 @@
+//
+//  RLReezyCell.m
+//  Reezy
+//
+//  Created by Richard on 2014-07-31.
+//  Copyright (c) 2014 Richard Lesco. All rights reserved.
+//
+
+#import "RLReezyCell.h"
+
+@implementation RLReezyCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
