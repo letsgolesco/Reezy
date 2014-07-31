1b2838f4bc2acbb4d70349e1aa42dadeca0072a3
Richard Lesco <richardlesco@gmail.com>
2014-07-31 12:01:48 -0400

    Initial Commit

---
+++ Reezy/RLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RLAppDelegate.h
+//  Reezy
+//
+//  Created by Richard on 2014-07-31.
+//  Copyright (c) 2014 Richard Lesco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Reezy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Reezy
+//
+//  Created by Richard on 2014-07-31.
+//  Copyright (c) 2014 Richard Lesco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RLAppDelegate class]));
+    }
+}
